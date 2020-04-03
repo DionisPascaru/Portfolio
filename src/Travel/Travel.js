afda2ad92b5e436f666d7c5733cc6506ff8ce542
DionisPascaru <denis.pascaru95@gmail.com>
2020-04-03 22:02:31 +0300

    April 03 2020 - test commit

--- src/Travel/Travel.js
+++ src/Travel/Travel.js
@@ -5,31 +5,13 @@ import TravelContent from './travel-content';
 
 
 function Travel() {
-
-    const [toggled, toggle] = useState(false);
-
-    useEffect(()=>{
-        var header = document.getElementById("travel");
-        var listContent = header.getElementsByClassName("crop-image");
-        console.log(listContent)
-        for (var i = 0; i < listContent.length; i++) {
-            listContent[i].addEventListener("click", function() {
-                var current = document.getElementsByClassName("active");
-                if (current.length > 0) { 
-                    current[0].className = current[0].className.replace(" active", "");
-                }
-                if(toggled === true){
-                    this.className += "active";
-                } else {
-                    this.className = delete("active");
-                }                
-            });
-        }
-    })
-
+    let myRef = React.createRef();
+    let openContent = () => {
+        console.log("hello", myRef);
+    }
+    
     return (
-        <div className={`travel-container ${toggled ? 'active' : ''}`}>
-
+        <div className="travel-container ">
             <div className="travel-section sticky">
                 <img src={images.travelIcon} />
                 <div className="nav-bar" >
@@ -51,11 +33,11 @@ function Travel() {
                 </div>
             </div>
             <div id="travel" className="travel-chapter">
-                <div className="crop-image">
+                <div className="crop-image" ref={myRef}>
                     <div className="image-wrap">
                         <div className="chapter-subtitle">
                             <h2>Sankt Moritz</h2>
-                            <div onClick={() => toggle(toggled => !toggled)}>
+                            <div onClick={openContent}>  
                                 More
                             </div>
                         </div>
@@ -65,11 +47,11 @@ function Travel() {
                         <TravelContent />
                     </div>
                 </div>
-                <div className="crop-image">
+                <div className="crop-image" ref={myRef}>
                     <div className="image-wrap">
                         <div className="chapter-subtitle">
                             <h2>Livigno</h2>
-                            <div onClick={() => toggle(toggled => !toggled)}>
+                            <div onClick={openContent}>
                                 More
                             </div>
                         </div>
@@ -79,11 +61,11 @@ function Travel() {
                         <TravelContent />
                     </div>
                 </div>
-                <div className="crop-image">
+                <div className="crop-image" ref={myRef}>
                     <div className="image-wrap">
                         <div className="chapter-subtitle">
                             <h2>Milano</h2>
-                            <div onClick={() => toggle(toggled => !toggled)}>
+                            <div onClick={openContent}>
                                 More
                             </div>
                         </div>
